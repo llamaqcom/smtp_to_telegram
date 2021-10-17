8ce6388d2495bc2aeda973b6090775d22b717120
Alec Petrosky <alec@alecpetrosky.com>
2021-10-17 15:52:57 -0500

    Healthcheck enhanced, especially for workstations without permanent internet connection.

--- check-running.sh
+++ check-running.sh
@@ -2,4 +2,6 @@
 
 set -eu
 
-nc -w 3 -z 127.0.0.1 2525 && nc -w 5 -z api.telegram.org 443 || exit 1
+nc -w 3 -z 127.0.0.1 2525 || exit 1
+nc -w 5 -z cloudflare.com 443 || exit 0
+nc -w 5 -z api.telegram.org 443 || exit 1
